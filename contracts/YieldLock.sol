a7456d3b3020a533d1aa591f19a42751edef20fb
Sujal Verma <98179303+vermasujal841@users.noreply.github.com>
2025-07-17 23:02:39 +0530

    Update YieldLock.sol

--- contracts/YieldLock.sol
+++ contracts/YieldLock.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+  
 contract YieldLock is ReentrancyGuard, Ownable {
     IERC20 public stakingToken;
     IERC20 public rewardToken;
